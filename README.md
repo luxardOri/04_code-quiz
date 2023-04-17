ad7b3942df64f912f64d7bd5be792e1ea29be046
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-04-16 22:34:13 -0700

    committing what I've got so I can at least turn in the bare minimum.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Week-4-Challange
+
+## Description
+
+This is a timed quiz. With javascript skills, I am to make a quiz that is timed, that will hold high scores in local storage. There should be a variety of questions and users are to select from multiple answers. Users will be penalized by reducing time from the timer, if they get an answer wrong.
+
+## Installation
+
+No installations needed, but will need access to final project link and github. That will be provided in the Usage section.
+
+## Usage
+
+To see the final project please see the links below:
+
+GitHub link: https://github.com/luxardOri/Week-4-Challenge/  
+Final project link: https://luxardori.github.io/Week-4-Challenge/
+
+You can see the html used by either clicking on the gitHub link or by inspecting the final project link.
+
+
+## Credits
+
+Here are the websites I've referenced:  
+
+I haven't used this yet, but have been reading through for personal reference to see what other people did:  
+https://codepen.io/boopalan002/pen/yKZVGa  
--- script.js
+++ script.js
@@ -1,68 +1,83 @@
+// Questions for the quiz
 const questions = [
     {
         question: "What is the main ingredient in meringues?",
-        answers: {
-            one: "Sugar",
-            two: "Water",
-            three: "Egg yolks",
-            four: "Egg whites"
-        },
-        correctAnswer: "4"
+        answers: ["Sugar","Water","Egg yolks","Egg whites"],
+        correctAnswer: 3
+    },{
+        question: "What is typically NOT used in making caramel?",
+        answers: ["Honey","Cream","Sugar","Butter"],
+        correctAnswer: 0
+    },{
+        question: "Which of the following desserts are made of puff pastry?",
+        answers: ["Cannele","Palmier","Pudim de leite","Croquembouche"],
+        correctAnswer: 1
     },{
         question: "First question?",
-        answers: {
-            one: "answer",
-            two: "answer",
-            three: "answer",
-            four: "answer"
-        },
-        correctAnswer: "3"
+        answers: ["answer","answer","answer","answer"],
+        correctAnswer: 2
     },{
         question: "First question?",
-        answers: {
-            one: "answer",
-            two: "answer",
-            three: "answer",
-            four: "answer"
-        },
-        correctAnswer: "3"
+        answers: ["answer","answer","answer","answer"],
+        correctAnswer: 2
     },{
         question: "First question?",
-        answers: {
-            one: "answer",
-            two: "answer",
-            three: "answer",
-            four: "answer"
-        },
-        correctAnswer: "3"
+        answers: ["answer","answer","answer","answer"],
+        correctAnswer: 2
     },{
         question: "First question?",
-        answers: {
-            one: "answer",
-            two: "answer",
-            three: "answer",
-            four: "answer"
-        },
-        correctAnswer: "3"
-    },
+        answers: ["answer","answer","answer","answer"],
+        correctAnswer: 2
+    },{
+        question: "First question?",
+        answers: ["answer","answer","answer","answer"],
+        correctAnswer: 2
+    },{
+        question: "First question?",
+        answers: ["answer","answer","answer","answer"],
+        correctAnswer: 2
+    },{
+        question: "First question?",
+        answers: ["answer","answer","answer","answer"],
+        correctAnswer: 2
+    }
 ]
-
-var score = 0;
-var timer = 0;
 
 var startButton = $('#start-button');
 var startBlock = $('#start-block');
 var quizBlock = $('#quiz-block');
+var showQuest = $('#question');
+var showAns = $('#answers');
 
-function startGame() {
-    timer = 100;
-    console.log(timer);
+var score = 0;
+var timer = 0;
+
+// This function is used to set the timer and will use setInterval to get timer to work
+function setTimer() {
+    timer = 120;
+};
+
+// Will use this function to set the score for the game. I will need to set it so it will store in local storage, so even if I reload the page it will show who has the highest score.
+function setScore() {
+    score = 0;
 }
 
-startButton.click(function() {
+// This will show the questions and answers to be selected. 
+function showQuestions() { 
+    showQuest.text(questions[0].question);
+
+    questions[0].answers.forEach(answers => {
+        showAns.text(questions[0].answers);
+    });
+
+};
+
+// When user clicks on the start button, this hides the main page and starts the quiz
+startButton.on('click', function() {
     quizBlock.removeClass('hide');
     quizBlock.addClass('show');
     startBlock.removeClass('show');
     startBlock.addClass('hide');
-    startGame();
+    showQuestions();
 });
+
